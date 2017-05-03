c8ab2aa2b54a7fa91d02248b6f5bc2420d6fc017
alexa <alexapavlovich@gmail.com>
2017-05-03 15:55:39 +0200

    DistinctCompleted

--- NorthwindWPF/NorthwindWPF/viewmodel.cs
+++ NorthwindWPF/NorthwindWPF/viewmodel.cs
@@ -17,10 +17,106 @@ namespace NorthwindWPF.context
             get { return db.Employees.ToList(); }
         }
        
-       public List<string> Alltits
+        public List<string> Alltits
         {
             get { return AllEmp.Select(e => e.Title).Distinct().ToList(); } 
         }
+
+        public List<string> AllTcs
+        {
+            get { return AllEmp.Select(e => e.TitleOfCourtesy).Distinct().ToList(); }
+        }
+
+        public List<string> AllCts
+        {
+            get { return AllEmp.Select(e => e.City).Distinct().ToList(); }
+        }
+
+        public List<string> AllRgn
+        {
+            get { return AllEmp.Select(e => e.Region).Distinct().ToList(); }
+        }
+
+        public List<string> AllPsc
+        {
+            get { return AllEmp.Select(e => e.PostalCode).Distinct().ToList(); }
+        }
+
+        public List<string> AllCtr
+        {
+            get { return AllEmp.Select(e => e.Country).Distinct().ToList(); }
+        }
+
+        public List<string> AllCont
+        {
+            get { return AllCus.Select(e => e.ContactTitle).Distinct().ToList(); }
+        }
+
+        public List<string> AllEcit
+        {
+            get { return AllCus.Select(e => e.City).Distinct().ToList(); }
+        }
+
+        public List<string> AllEreg
+        {
+            get { return AllCus.Select(e => e.Region).Distinct().ToList(); }
+        }
+
+        public List<string> AllEpos
+        {
+            get { return AllCus.Select(e => e.PostalCode).Distinct().ToList(); }
+        }
+
+        public List<string> AllEcon
+        {
+            get { return AllCus.Select(e => e.Country).Distinct().ToList(); }
+        }
+
+        public List<string> AllCt
+        {
+            get { return AllSup.Select(e => e.ContactTitle).Distinct().ToList(); }
+        }
+
+        public List<string> AllSct
+        {
+            get { return AllSup.Select(e => e.City).Distinct().ToList(); }
+        }
+
+        public List<string> AllSReg
+        {
+            get { return AllSup.Select(e => e.Region).Distinct().ToList(); }
+        }
+
+        public List<string> AllSps
+        {
+            get { return AllSup.Select(e => e.PostalCode).Distinct().ToList(); }
+        }
+
+        public List<string> AllSCtry
+        {
+            get { return AllSup.Select(e => e.Country).Distinct().ToList(); }
+        }
+
+        public List<string> AllScty
+        {
+            get { return AllOrd.Select(e => e.ShipCity).Distinct().ToList(); }
+        }
+
+        public List<string> AllOsr
+        {
+            get { return AllOrd.Select(e => e.ShipRegion).Distinct().ToList(); }
+        }
+
+        public List<string> AllOhs
+        {
+            get { return AllOrd.Select(e => e.ShipPostalCode).Distinct().ToList(); }
+        }
+
+        public List<string> AllOctry
+        {
+            get { return AllOrd.Select(e => e.ShipCountry).Distinct().ToList(); }
+        }
+
         public List<Category> AllCat
         {
             get { return db.Categories.ToList(); }
