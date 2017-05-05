b233d62d28f4f41ad5a9746fa9d43e8ecbcc756a
Roland <HAI16954@spengergasse.at>
2017-05-05 22:19:55 +0200

    Chart is displayed

--- NorthwindWPF/NorthwindWPF/Overview.xaml.cs
+++ NorthwindWPF/NorthwindWPF/Overview.xaml.cs
@@ -23,10 +23,29 @@ namespace NorthwindWPF
     public partial class Overview : Window
     {
 
+      
         public Overview()
         {
             InitializeComponent();
+
+            PointLabel = chartPoint =>
+                string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+
+            DataContext = this;
         }
 
+        public Func<ChartPoint, string> PointLabel { get; set; }
+
+        private void Chart_OnDataClick(object sender, ChartPoint chartpoint)
+        {
+            var chart = (LiveCharts.Wpf.PieChart)chartpoint.ChartView;
+
+            //clear selected slice.
+            foreach (PieSeries series in chart.Series)
+                series.PushOut = 0;
+
+            var selectedSeries = (PieSeries)chartpoint.SeriesView;
+            selectedSeries.PushOut = 8;
+        }
     }
 }
